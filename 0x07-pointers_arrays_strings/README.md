000076b014ec3d9d4d3e145bc0ae8fbaf4c44964
techbywes <dhspotless@gmail.com>
2023-03-06 03:56:02 -0800

    update

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,1 @@
 0x07. C - Even more pointers, arrays and strings
-
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,14 +1,15 @@
-#ifndefMAIN_H
-#defineMAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-char*_memset(char*s,charb,unsignedintn);
-int_putchar(charc);
-char*_memcpy(char*dest,char*src,unsignedintn);
-char*_strchr(char*s,charc);
-unsignedint_strspn(char*s,char*accept);
-char*_strpbrk(char*s,char*accept);
-char*_strstr(char*haystack,char*needle);
-voidprint_chessboard(char(*a)[8]);
-voidprint_diagsums(int*a,intsize);
-voidset_string(char**s,char*to);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif
+
