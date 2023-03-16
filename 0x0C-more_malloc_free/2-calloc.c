fafa0ad9af08c4edeb1dc5f1b409c5991d8ffd07
techbywes <dhspotless@gmail.com>
2023-03-16 06:22:19 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,18 +10,18 @@
 *
 * Return: pointer to the allocated memory.
 */
-void * _calloc(unsigned int nmemb, unsigned int size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-   char *a;
-   unsigned int b;
+char *a;
+unsigned int b;
 
 
-   if (nmemb == 0 || size == 0)
-       return (NULL);
-   a = malloc(nmemb * size);
-   if (a == NULL)
-       return (NULL);
-   for (b = 0; b < (nmemb * size); b++)
-       a[b] = 0;
-   return (a);
+if (nmemb == 0 || size == 0)
+return (NULL);
+a = malloc(nmemb * size);
+if (a == NULL)
+return (NULL);
+for (b = 0; b < (nmemb * size); b++)
+a[b] = 0;
+return (a);
 }
